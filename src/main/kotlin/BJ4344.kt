2360c7e99815e322b11708dd513d5129c2fed90c
SJ <mh050210@namuplanet.com>
2025-05-28 22:41:20 +0900

    [New] 백준 4344 풀이

---
+++ src/main/kotlin/BJ4344.kt
@@ -0,0 +1,22 @@
+class BJ4344 {
+    fun main() = with(System.`in`.bufferedReader()) {
+        val c = readLine().toInt()
+        for (i in 0 until c) {
+            val line = readLine().split(" ")
+            val n = line.first().toInt()
+
+            var sum = 0
+            for (i in 1..n)
+                sum += line[i].toInt()
+
+            val avg = sum / n.toFloat()
+            var cnt = 0
+            for (i in 1..n) {
+                if (line[i].toFloat() > avg)
+                    cnt++
+            }
+            val overAvgRatio = cnt / n.toFloat() * 100F
+            println("${"%.3f".format(overAvgRatio)}%")
+        }
+    }
+}
