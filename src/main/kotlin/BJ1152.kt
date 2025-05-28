07cf60bf35380dbfc0c1cec99230a876096b6658
SJ <mh050210@namuplanet.com>
2025-05-28 23:05:29 +0900

    [Fix] 백준 1152 최적화

--- src/main/kotlin/BJ1152.kt
+++ src/main/kotlin/BJ1152.kt
@@ -1,9 +1,7 @@
+import java.util.StringTokenizer
+
 class BJ1152 {
     fun main() = with(System.`in`.bufferedReader()) {
-        val str = readLine().trimStart().trimEnd().split(' ')
-        if (str == listOf(""))
-            print('0')
-        else
-            print(str.size)
+        print(StringTokenizer(readLine()).countTokens())
     }
 }
